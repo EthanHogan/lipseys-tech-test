576d0a0f48875ebb0df516c0fc96e90e4b2935b9
EthanHogan <ethan.hogan7@gmail.com>
2023-06-25 13:28:20 -0500

    removes unused import

--- client/src/components/DuplicateEmails.tsx
+++ client/src/components/DuplicateEmails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, Col, Form, Row, Table } from "react-bootstrap";
 import { motion } from "framer-motion";
 
